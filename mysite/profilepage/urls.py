944e284db9aaf1479fc9cc3056cbec28569c8ed5
Daequan <daequanr@buffalo.edu>
2022-04-04 11:30:46 -0400

    updated html & css

--- mysite/profilepage/urls.py
+++ mysite/profilepage/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from profilepage.views import Index
+from profilepage.views import Idk
+from . import views
 
 urlpatterns = [
-    path('', Index.as_view(), name='profile_index')
+    path('', Index.as_view(), name='profile_index'),
+    #path('settings/', Index.profileSettings(GET), name='form_page')
+    path('settings/',Idk.as_view(), name='edit_profile')
 ]
--- mysite/profilepage/views.py
+++ mysite/profilepage/views.py
@@ -1,8 +1,17 @@
+from email.policy import HTTP
+from http.client import HTTPResponse
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+
+class Idk(View):
+    def get(self, request, *args, **kwargs):
+        return render(request,'profileHome/editProfile.html')
 
 class Index(View):
     #def profile(request):
     def get(self, request, *args, **kwargs):
         return render(request, 'profileHome/profile.html')
+
 # Create your views here.
+    #return render_to_response('profileHome/editProfile.html')
