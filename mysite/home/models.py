680256e31a7e773d1f29c9f0712131beac183f0c
Jason Liu <jhliu4@buffalo.edu>
2022-04-04 14:49:32 -0400

    created model for listings - temporary

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+# from django.contrib.auth import User
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
