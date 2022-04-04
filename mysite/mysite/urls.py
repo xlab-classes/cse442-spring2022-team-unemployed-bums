b626f48bea5c424cb627e973387d74d9a5d34f44
Jason Liu <jhliu4@buffalo.edu>
2022-04-04 16:31:19 -0400

    reverted urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -30,7 +30,4 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "Register/password_reset_sent.html"), name = "password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = "Register/password_reset_form.html"), name = "password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "Register/password_reset_done.html"), name = "password_rest_complete"),
-    path('profile/', include("profilepage.urls")),
-    path('home/', include('home.urls')),
-    path('listingcreation/', include('listingcreation.urls')),
 ]
