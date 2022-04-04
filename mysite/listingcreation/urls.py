6ff1a904d82a675b1e86f2f9c01ed4462e7cddf2
Jason Liu <60624849+jay3393@users.noreply.github.com>
2022-04-04 16:33:26 -0400

    Revert "merge local sql"

--- mysite/listingcreation/urls.py
+++ mysite/listingcreation/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path
+from django.urls import include
+from . import views
 
-
+appname = 'listingcreation'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("django.contrib.auth.urls")),
-    path('profile/', include("profilepage.urls")),
-    path('home/', include('home.urls')),
-    path('listingcreation/', include('listingcreation.urls'))
+    path('', views.creationpage, name = 'listingcreationindex'),
+    path('submission/', views.listingsubmission),
 ]
