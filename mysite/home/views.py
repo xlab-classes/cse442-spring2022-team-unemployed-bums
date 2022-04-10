fb05ff872bcaa8ece07fe3ac75ea5c1bf92177c0
Jason Liu <jhliu4@buffalo.edu>
2022-04-10 18:26:18 -0400

    #87 added posting information

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,5 @@
 from cgitb import html
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
@@ -49,7 +50,7 @@ listings = [
     },
     {
         'id': '3462918370',
-        'name': "Ursulina",
+        'author': "Ursulina",
         'title': "Ben's Barbeque",
         'content': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas nec sem at mattis. Vivamus cursus dolor sit amet nibh finibus, nec fringilla erat finibus. Nam pellentesque arcu eget purus varius aliquet. Fusce ornare eget diam vel elementum. Fusce tincidunt id eros ut congue. Suspendisse vulputate ut tortor vitae lacinia. Mauris urna nulla, semper vitae mauris sit amet, pretium iaculis ligula. Nunc et est tristique, condimentum tellus vulputate, pharetra ipsum.",
         'date_posted': "03/13/2022",
@@ -57,7 +58,7 @@ listings = [
     },
     {
         'id': '1293827209',
-        'name': "Jean",
+        'author': "Jean",
         'title': "Movie Night (FREE FOOD)",
         'content': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus dolor nibh. Nullam sit amet turpis at sapien tempor eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam sodales vulputate enim interdum feugiat. Sed vitae fermentum magna. Fusce rhoncus est orci, non sollicitudin tortor mollis ut. Praesent consectetur ultricies leo vitae tincidunt. Pellentesque ultrices, quam eu aliquam scelerisque, sapien mi tincidunt felis, et aliquam justo arcu vitae nisl. Nulla lorem leo, facilisis sed sagittis eget, egestas vitae augue. In hac habitasse platea dictumst. Nulla quis nunc mi. Maecenas in sollicitudin elit.",
         'date_posted': "03/13/2022",
@@ -65,7 +66,37 @@ listings = [
     },
 ]
 
-def get_event_info():
+def get_listing(listing_id):
+    for listing in listings:
+        print(listing)
+        if listing['id'] == listing_id:
+            return listing
+    return None
+
+def get_event_info(username, listing_id):
+    listing = get_listing(listing_id)
+    if listing:
+        print(listing)
+        author = listing['author']
+        title = listing['title']
+        content = listing['content']
+        event_date = listing['event_date']
+
+        return (
+            f"""Thank you for the RSVP {username}!\n
+You have RSVP'd to "{title}" - posted by {author}\n
+\n
+Description\n
+{content}\n
+\n
+Event Date {event_date}
+
+EVNT RSVP Notifications
+            """
+        )
+        
+
+
     return 'this is the event info'
 
 def index(request):
@@ -77,12 +108,14 @@ def index(request):
     if request.method == 'GET':   
         return render(request, 'home/index.html', context)
     else:
-        items = request.POST.items()
+        items = dict(list(request.POST.items()))
+        print(items)
         key, value = items
         # send email to user here
+        # send_mail(get_event_info(request.user, items['listing_id']))
         send_mail(
             subject='You RSVP\'d to an event!',
-            message=get_event_info(),
+            message=get_event_info(request.user, items['listing_id']),
             from_email='EVNT RSVP',
             recipient_list=[request.user.email],
         )
