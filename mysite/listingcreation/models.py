73ed26b69f030365d85b352a06b71864d18d863d
Jason Liu <jhliu4@buffalo.edu>
2022-04-11 03:35:48 -0400

    #87 working eventdate

--- mysite/listingcreation/models.py
+++ mysite/listingcreation/models.py
@@ -3,8 +3,8 @@ import uuid
 from django.utils import timezone
 
 search_tag_choices = ['Outdoors', 'sports', 'recreational', 'edjucational']
-daychoices = [('1', '1'),('2', '2'),('3', '4'),('5', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10'),('11', '11'),('12', '12'),('13', '13'),('14', '14'),('15', '15'),('16', '16'),('17', '17'),('18', '18'),('19', '19'),('20', '20'),('21', '21'),('22', '22'),('23', '23'),('24', '24'),('25', '25'),('26', '26'),('27', '27'),('28', '28'),('29', '29'),('30', '30'),('31', '31')]
-monthchoices = [('1', '1'),('2', '2'),('3', '4'),('5', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10'),('11', '11'),('12', '12')]
+daychoices = [('1', '1'),('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10'),('11', '11'),('12', '12'),('13', '13'),('14', '14'),('15', '15'),('16', '16'),('17', '17'),('18', '18'),('19', '19'),('20', '20'),('21', '21'),('22', '22'),('23', '23'),('24', '24'),('25', '25'),('26', '26'),('27', '27'),('28', '28'),('29', '29'),('30', '30'),('31', '31')]
+monthchoices = [('1', '1'),('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10'),('11', '11'),('12', '12')]
 yearchoices = [('2022', '2022'),('2023', '2023'),('2024', '2024')]
 
 class ListingCreationModel(models.Model):
@@ -13,9 +13,7 @@ class ListingCreationModel(models.Model):
     author = models.CharField(max_length=200, default="anonymous")
     created_on = models.DateTimeField(default=timezone.now)
     description = models.TextField()
-    eventday = models.IntegerField(default='1')
-    eventmonth = models.IntegerField(default='1')
-    eventyear = models.IntegerField(default='2022')
+    eventdate = models.CharField(max_length=10, default="4/11/2022")
     outdoors = models.BooleanField()
     sports = models.BooleanField()
     recreation = models.BooleanField()
