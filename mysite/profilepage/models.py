c2afa15fddcdadf744a74f6c1082c25af9bd0f75
Jason Liu <jhliu4@buffalo.edu>
2022-04-24 02:48:39 -0400

    #89 created follow/unfollow button that actually follows and unfollows users, also filtered posts to display only user created posts

--- mysite/profilepage/models.py
+++ mysite/profilepage/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Follow (models.Model):
-    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    following = models.ManyToManyField(User, related_name='following')
+    user = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    following_user = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
 
-    # def __repr__(self):
-    #     return "User ({})".format(user.)
+    def __str__(self):
+        return "Follow ({} -> {})".format(self.user.username, self.following_user.username)
+
+    class Meta:
+        unique_together = ['user', 'following_user']
