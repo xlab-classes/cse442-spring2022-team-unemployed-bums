0538dbf0c2faf987bcfe0d06c70c3cd71a165731
Jason Liu <jhliu4@buffalo.edu>
2022-04-25 12:41:14 -0400

    #89 added follow model

--- mysite/Register/models.py
+++ mysite/Register/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 # Create your models here.
+class Follow (models.Model):
+    user = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    following_user = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Follow ({} -> {})".format(self.user.username, self.following_user.username)
+
+    class Meta:
+        unique_together = ['user', 'following_user']
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
