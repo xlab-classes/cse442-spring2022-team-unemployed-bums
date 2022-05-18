c04442ba8357f03c32624a430c1149f9d50fc930
couchkevin <60620553+couchkevin@users.noreply.github.com>
2022-05-18 16:27:21 -0400

    #12 Changed Settings.py
    Made minor changes to the file to get rid of email config settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -165,10 +165,10 @@ LOGIN_URL = "/login"
 
 
 # SMTP Email Configuration 
-DEFAULT_FROM_EMAIL = 'unemployedbumsnotification@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'unemployedbumsnotification@gmail.com'
-EMAIL_HOST_PASSWORD = 'whatapassword1234!'
+#DEFAULT_FROM_EMAIL = ''
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_USE_TLS = True
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD = ''
